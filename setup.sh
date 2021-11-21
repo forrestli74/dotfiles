78eefc80c9b2b3aac2b373b059ac148686682ac4
Jiaqi Li <lijiaqigreat@gmail.com>
2021-11-21 07:59:16 -0800

    remove broken tmux commands

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-. ./rcrc
 for f in $(ls $DOTFILES_DIRS 2> /dev/null)
 do
   if [[ "$EXCLUDES" != *"$f"* ]] && [[ "$f" != *"/"* ]] && [ ! -L ~/.$f ]; then
     ln -s ~/dotfiles/$f ~/.$f
   fi
 done
+chsh -s $(which bash)
+
